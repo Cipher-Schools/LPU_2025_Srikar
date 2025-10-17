8c8aed2c63ee2deb82d45ccd00f627b2c00c9907
unknown <srikarrohit@gmail.com>
2025-10-17 10:53:23 +0530

    functions and recursion

--- Cpp/functions.cpp
+++ Cpp/functions.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
+int add(int x, int y=5);//default parameter
+void swap(int *x, int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+void swap1(int &x,int &y){
+    int temp=x;
+    x=y;
+    y=temp;
+}
+int test(int (&arr)[5]);
 int main(){
-    
+    std::cout<<add(2)<<std::endl;
+    double pi=3.1415926359;
+    std::cout<<std::scientific<<pi<<std::endl;
+    std::cout<<std::setprecision(2)<<pi<<std::endl;
+    int a=3,b=5;
+    swap1(a,b);
+    std::cout<<a<<" "<<b<<std::endl;
+}
+inline int add(int x, int y){
+    return x+y;
 }
---
+++ Cpp/recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int fib(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    return fib(n-1)+fib(n-2);
+}
+int fact(int n){
+    if(n==0)
+        return 1;
+    return n*fact(n-1);
+}
+int main(){
+    cout<<fact(5)<<endl;
+}
