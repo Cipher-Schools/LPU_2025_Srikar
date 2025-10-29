76421ac4a199b104307940d9ded766f37547d921
unknown <srikarrohit@gmail.com>
2025-10-29 11:52:12 +0530

    pointers

--- Cpp/pointers.cpp
+++ Cpp/pointers.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 using namespace std;
-typedef struct{
+struct Dog{
     int age;
     string name;
-}Dog;
+    Dog(int a,string name){
+        age=a;
+        this->name=name;
+        cout<<"Dog created"<<endl;
+    }
+};
 class Man{
     public:
         int age;
         string name;
-        Man(){//same name as the class name
+        Man(){}
+        Man(int age,string name){//same name as the class name
+            this->age=age;
+            this->name=name;
             cout<<"Man is created"<<endl;
         }
 };
 int main(){
     int a=10;
     int *ptr=&a;//declaration & initialization of a pointer
-    Dog dog;//no need of new or malloc
+    Dog dog(10,"Snoofy");//no need of new or malloc
     Dog* dogptr=&dog;
-    Man man;
+    dogptr->age=10;
+    Dog* dogptr1=new Dog(15,"Chichu");
+    Man man(25,"Rohit");
     Man* manptr=&man;
     (*manptr).age=20;
     manptr->name="Rohit";
-    Man *manptr1=new Man();//instead of malloc
+    Man *manptr1=new Man(15,"Sharma");//instead of malloc
     int arr[5];
     int *arrptr=arr;//&arr[0]
     int *arrptr1=new int[10];//block of 10*4 bytes is allocated on heap
