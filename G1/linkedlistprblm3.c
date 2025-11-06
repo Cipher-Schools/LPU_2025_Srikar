ca06c189cfd375ccef227d6e4f40ce61087eff45
unknown <srikarrohit@gmail.com>
2025-11-06 11:52:33 +0530

    Lecture 9 problems

---
+++ G1/linkedlistprblm3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+    int data;
+    struct Node* next;
+}Node;
+Node* createNode(int data){
+    Node* newNode=malloc(sizeof(Node));
+    newNode->data=data;
+    newNode->next=NULL;
+    return newNode;
+}
+Node* insertAtEnd(Node* head,int data){
+    if(head==NULL){
+        return createNode(data);
+    }
+    Node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=createNode(data);
+    return head;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    Node* head=NULL;
+    for(int i=0;i<n;i++){
+        int data;
+        scanf("%d",&data);
+        head=insertAtEnd(head,data);
+    }   
+    Node* curr=head;
+    Node* next=curr->next;
+    while(curr!=NULL && next!=NULL){
+        int data1=curr->data;
+        int data2=next->data;
+        curr->data=data2;
+        next->data=data1;
+	if(next!=NULL)
+       		curr=next->next;
+	if(curr!=NULL)
+        	next=curr->next;
+    }
+    Node* temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
